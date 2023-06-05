71f98bd8dcefdce475405f72ca3fb67334bd3a82
Semi Invader <=>
2023-06-05 12:31:21 -0600

    lottery buys working

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,20 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "./interfaces/ILottery.sol";
+// TODO REMOVE TEST LIBRARY
+import "hardhat/console.sol";
 
-contract InfiniteLottery is ILottery, VRFConsumerBaseV2 {
+error MinimumTicketsNotReached(uint minTickets);
+error HighValue(uint valueSent);
+
+contract InfiniteLottery is ILottery, VRFConsumerBaseV2, Ownable {
     struct Level1Participants {
         address[] users;
-        uint[] userTickets;
+        uint[] ticketsPerUser;
+        uint totalTickets;
+        uint roiOverflow;
     }
     struct UpperLevels {
         uint minL1;
         uint maxL1;
         bytes32 vrfRequestId;
+        uint currentPot;
     }
     struct WinnerInfo {
         uint[] levels;
@@ -29,29 +38,34 @@ contract InfiniteLottery is ILottery, VRFConsumerBaseV2 {
     struct UserRoundInfo {
         uint tickets;
         uint index; // Index in the users array for Level1Participants
+        bool set;
     }
     mapping(uint _level => mapping(uint _id => UpperLevels))
         private _higherLevels;
-    mapping(uint _level => mapping(uint _id => UserRoundInfo))
-        public userTickets;
     mapping(uint _round => Level1Participants) private _level1;
-    mapping(address _user => UserParticipations) public userPartipations;
+    mapping(address _user => UserParticipations) public userParticipations;
+    mapping(address _user => mapping(uint _level1Id => UserRoundInfo))
+        public userTickets;
     //TODO Make sure we have the winner info visible on request
     mapping(bytes32 randomRequest => WinnerInfo) private winnerInfo;
+    mapping(uint _level => uint maxId) public maxRoundIdPerLevel;
+
+    uint[] public roundsToPlay; // Only level_1 IDs here.
 
     IERC20 public USDC; // The only Token used here
+    // BSC USDC address = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d
     VRFCoordinatorV2Interface private immutable vrf;
     address public dividendNFT;
     address public discountLocker;
     address public winnerDistribution;
     address public teamWallet;
 
-    uint public currentL1; // Advances when each L1 advances
     uint public currentHighest; // Current highest with prize pot associated with it.
     uint public currentWeeklyRound;
     uint public constant ticketsPerL1 = 1000;
     uint public constant l1Advancement = 20; // 20 rounds of L1 for a L2
     uint public constant upperAdvancement = 10; // 10 rounds of any upper level to the next level
+    uint public immutable ticketPrice;
     uint public winnerPot = 30;
     uint public rollupPot = 30;
     uint public roiPot = 20;
@@ -59,6 +73,8 @@ contract InfiniteLottery is ILottery, VRFConsumerBaseV2 {
     uint public referralPot = 6;
     uint public teamPot = 2;
     uint public weeklyPot = 2;
+    uint public MINIMUM_TICKETS_PER_BUY = 10;
+
     // VRF Stuff
     bytes32 private vrfHash;
     uint64 private subid;
@@ -75,7 +91,237 @@ contract InfiniteLottery is ILottery, VRFConsumerBaseV2 {
     // 0x6A2AAd07396B36Fe02a22b33cf443582f682c82f
     // VRF 50gwei key hash
     // 0xd4bb89654db74673a187bd804519e65e3f71a52bc55f11da7601a13dcf505314
-    constructor(address _vrfCoordinator) VRFConsumerBaseV2(_vrfCoordinator) {
+    constructor(
+        address _vrfCoordinator,
+        address _usdc,
+        uint _ticketPrice
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
         vrf = VRFCoordinatorV2Interface(_vrfCoordinator);
+        USDC = IERC20(_usdc);
+        ticketPrice = _ticketPrice;
+    }
+
+    function startLottery() external onlyOwner {
+        require(maxRoundIdPerLevel[1] == 0, "Already started");
+        maxRoundIdPerLevel[1] = 1;
+        _higherLevels[2][1] = UpperLevels(1, 0, bytes32(0), 0);
+        emit LotteryStarted(block.timestamp);
+    }
+
+    function buyTickets(uint _ticketAmount, address _referral) external {
+        _buyTickets(_ticketAmount, _referral, msg.sender);
+    }
+
+    function buyTicketsForUser(
+        uint _ticketAmount,
+        address _referral,
+        address _user
+    ) external {
+        _buyTickets(_ticketAmount, _referral, _user);
+    }
+
+    function _buyTickets(
+        uint _ticketAmount,
+        address _referral,
+        address _user
+    ) private {
+        require(maxRoundIdPerLevel[1] > 0, "Not started");
+        if (_ticketAmount < 10) revert MinimumTicketsNotReached(10);
+        UserParticipations storage userPlays = userParticipations[_user];
+        uint leftovers;
+        uint roiTickets;
+        uint level1Played = maxRoundIdPerLevel[1];
+        UserRoundInfo storage userPlaying = userTickets[_user][level1Played];
+        // set referral
+        if (
+            userPlays.referral == address(0) &&
+            userPlays.referral != _user &&
+            _referral != address(0)
+        ) userPlays.referral = _referral;
+        // SET TICKETS THAT ARE BOUGHT
+        // TODO need to send roiLeftOver to a global value for Team wallet
+        (leftovers, level1Played, roiTickets) = _createTickets(
+            _ticketAmount,
+            level1Played,
+            true,
+            _user,
+            userPlaying
+        );
+        // OVERFLOW OF CURRENT ROUND
+        if (leftovers > 0) {
+            do {
+                userPlaying = userTickets[_user][level1Played];
+                (leftovers, level1Played, ) = _createTickets(
+                    leftovers,
+                    level1Played,
+                    false,
+                    _user,
+                    userPlaying
+                );
+            } while (leftovers > 0);
+        }
+        // NOW SET THE ROI TICKETS FOR THE NEXT ROUND
+        userPlaying = userTickets[_user][level1Played];
+        (leftovers, level1Played, ) = _createTickets(
+            roiTickets,
+            level1Played,
+            false,
+            _user,
+            userPlaying
+        );
+        // IF NEXT ROUND OVERFLOW... this scenario might not be needed
+        // but who really knows how crazy rich people think. Better safe than sorry
+        if (leftovers > 0) {
+            do {
+                userPlaying = userTickets[_user][level1Played];
+                (leftovers, level1Played, ) = _createTickets(
+                    leftovers,
+                    level1Played,
+                    false,
+                    _user,
+                    userPlaying
+                );
+            } while (leftovers > 0);
+        }
+
+        // Transfer money in
+        uint potAddition = _ticketAmount * ticketPrice;
+        console.log(
+            "request payment",
+            potAddition,
+            USDC.allowance(msg.sender, address(this))
+        );
+        USDC.transferFrom(msg.sender, address(this), potAddition);
+        // check roiPot amount, if it's not a multiple of 1 ether
+        // the remainder needs to be sent to  team wallet.
+    }
+
+    /// @notice Creates in a level for the user
+    /// @param amount ticket amount to create, can go above max l1 tickets
+    /// @param levelId the l1 id where tickets are added
+    /// @param generatesROI this should only be true once on buys, rest of time should be false
+    /// @return leftoverTickets the amount of tickets that overflow current round
+    /// @return nextId the next level 1 round
+    /// @return roiTickets the amount of tickets to claim for the next round
+    /// @dev roiLeftOver  the amount (in cents) of what should go to the discount pool
+    function _createTickets(
+        uint amount,
+        uint levelId,
+        bool generatesROI,
+        address _user,
+        UserRoundInfo storage userPlaying
+    ) private returns (uint leftoverTickets, uint nextId, uint roiTickets) {
+        uint roiLeftOver;
+        Level1Participants storage playLevel = _level1[levelId];
+        // Get the ROI values from here when necessary
+        if (generatesROI) {
+            roiTickets = (amount * roiPot) / 100;
+            roiLeftOver = (amount * ticketPrice * roiPot) / 100;
+            roiLeftOver = roiLeftOver % ticketPrice;
+        }
+        nextId = levelId + 1;
+        if (!userPlaying.set) {
+            userPlaying.index = playLevel.users.length;
+            userPlaying.set = true;
+            playLevel.users.push(_user);
+            playLevel.ticketsPerUser.push(0);
+            userParticipations[_user].participationsL1.push(levelId);
+        }
+        // get actual amount of tickets to use this time
+        leftoverTickets = playLevel.totalTickets + amount;
+        if (leftoverTickets >= ticketsPerL1) {
+            leftoverTickets = leftoverTickets - ticketsPerL1;
+            amount = ticketsPerL1 - playLevel.totalTickets;
+            userPlaying.tickets += amount;
+            playLevel.totalTickets = ticketsPerL1;
+            roundsToPlay.push(levelId);
+            maxRoundIdPerLevel[1]++;
+        } else {
+            userPlaying.tickets += amount;
+            playLevel.totalTickets = leftoverTickets; //overwrite value
+            leftoverTickets = 0;
+        }
+        playLevel.ticketsPerUser[userPlaying.index] = userPlaying.tickets;
+        if (roiLeftOver > 0) playLevel.roiOverflow += roiLeftOver;
+        emit TicketsBought(_user, levelId, amount);
+    }
+
+    // lets set this data when requesting random round info
+    // function queueHigherLevelToPlay(uint currentLevel, uint currentId) private {
+    //     bool advances;
+    //     uint requirement = currentLevel == 1 ? l1Advancement : upperAdvancement;
+    //     advances = currentId % requirement == 0;
+
+    //     if (advances) {
+    //         // set the maxL1 for the previous upper level ID
+    //         uint nextLevel = currentLevel + 1;
+    //         uint nextId = maxRoundIdPerLevel[nextLevel];
+    //         // set the max of current nextLevel
+    //         _higherLevels[nextLevel][nextId].maxL1 = currentId;
+    //         // get the next max Id to set the minimum
+    //         nextId ++;
+    //         // Set the minimum of the next level Round
+    //         if(currentLevel == 1) // if next Level is 2
+    //           _higherLevels[nextLevel][nextId].minL1 = currentId + 1;
+    //         else{
+    //           _higherLevels[nextLevel][nextId].minL1 = _higherLevels[currentLevel][currentId + 1].minL1;
+    //         }
+    //         // increase current ID
+    //         nextId = currentId + 1;
+    //         maxRoundIdPerLevel[currentLevel] = nextId;
+    //         emit AdvanceRound(currentId, nextId);
+    //         queueHigherLevelToPlay(nextLevel, nextId);
+    //     }
+    // }
+
+    /// @notice Give back the current amount pending
+    function ticketsToEndL1() public view returns (uint) {
+        uint currentL1Id = maxRoundIdPerLevel[1];
+        return ticketsPerL1 - _level1[currentL1Id].totalTickets;
+    }
+
+    function ticketsToEnd(uint _level) external view returns (uint) {
+        if (_level == 0) return 0;
+        if (_level == 1) return ticketsToEndL1();
+
+        uint currentMaxRound = maxRoundIdPerLevel[_level];
+        if (currentMaxRound == 0) currentMaxRound = 1;
+        uint currentL1inPlay = maxRoundIdPerLevel[1];
+        uint roundMultiplier = (10 ** (_level - 2)) * 20 * 1000;
+
+        currentMaxRound = roundMultiplier * currentMaxRound;
+        currentL1inPlay =
+            1000 *
+            currentL1inPlay +
+            _level1[currentL1inPlay].totalTickets -
+            ticketsPerL1;
+
+        return currentMaxRound - currentL1inPlay;
+    }
+
+    function setMinimumTicketBuy(uint _newAmount) external onlyOwner {
+        if (_newAmount > 100) revert HighValue(_newAmount);
+        emit SetNewMinimumTicketBuy(MINIMUM_TICKETS_PER_BUY, _newAmount);
+        MINIMUM_TICKETS_PER_BUY = _newAmount;
+    }
+
+    function fulfillRandomWords(
+        uint requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        console.log("Fulfill request", requestId, randomWords.length);
+    }
+
+    function allRoundsParticipatedIn(
+        address _user
+    ) external view returns (uint[] memory roundsPlayed) {
+        roundsPlayed = userParticipations[_user].participationsL1;
+    }
+
+    function ticketsL1OnRoundId(
+        uint _roundId,
+        address _user
+    ) external view returns (uint) {
+        return userTickets[_user][_roundId].tickets;
     }
 }
--- test/TestLottery.ts
+++ test/TestLottery.ts
@@ -1,7 +1,79 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { parseEther } from 'ethers/lib/utils';
+import { expect } from "chai";
 import { ethers } from 'hardhat'
 
+const zero = ethers.constants.AddressZero;
+
 describe("Lottery", function (){
-  describe("Intial state");
-  describe("Ticket Buys");
-  describe("Ticket Buys That trigger a win");
+  const setup = async () => {
+    const [owner, user1, user2, user3, placeholder] = await ethers.getSigners();
+    const MockUSDC = await ethers.getContractFactory("TestToken", owner);
+    const usdc = await MockUSDC.deploy();
+    const Lottery = await ethers.getContractFactory("InfiniteLottery", owner);
+    const lottery = await Lottery.deploy(placeholder.address, usdc.address, parseEther("1"));
+    //  Transfer USDC to users
+    await usdc.transfer(user1.address, parseEther("10001"));
+    await usdc.transfer(user2.address, parseEther("10001"));
+    await usdc.transfer(user3.address, parseEther("10001"));
+    return { owner, user1, user2, user3, placeholder, usdc, lottery };
+  }
+  describe("Intial state", function () {
+    it("should not allow to buy tickets", async () => {
+      const { lottery, user1, user2 } = await loadFixture(setup);
+      await expect(lottery.connect(user1).buyTickets(1, user2.address)).to.be.revertedWith("Not started");
+    })
+    it("should set the appropriate price", async () => {
+      const { lottery } = await loadFixture(setup);
+      expect(await lottery.ticketPrice()).to.equal(parseEther("1"));
+    })
+    it("should start with rounds at zero", async () => {
+      const { lottery } = await loadFixture(setup);
+      expect(await lottery.maxRoundIdPerLevel(1)).to.equal(0);
+      expect(await lottery.maxRoundIdPerLevel(2)).to.equal(0);
+      expect(await lottery.maxRoundIdPerLevel(3)).to.equal(0);
+    })
+  });
+  const lotteryStart = async () => {
+    const init = await setup();
+    await init.lottery.connect(init.owner).startLottery();
+    return init;
+  }
+  describe("On ticket buys", () => {
+    it("should allow to buy tickets and set roi tickets", async () => {
+      const { lottery, usdc, user1, user2 } = await loadFixture(lotteryStart);
+      
+      expect(await lottery.maxRoundIdPerLevel(1)).to.equal(1);
+      await expect( lottery.connect(user1).buyTickets(1, user2.address)).to.be.revertedWithCustomError(lottery, "MinimumTicketsNotReached").withArgs(10);
+      await expect( lottery.connect(user1).buyTickets(10, user2.address)).to.be.revertedWith("ERC20: insufficient allowance");
+
+      await usdc.connect(user1).approve(lottery.address, parseEther("10000"));
+
+      await lottery.connect(user1).buyTickets(10, zero);
+
+      expect(await lottery.userParticipations(user1.address)).to.equal(zero);
+      const participations = await lottery.allRoundsParticipatedIn(user1.address)
+      const round1L1U1 = await lottery.ticketsL1OnRoundId(1, user1.address)
+      const round2L1U1 = await lottery.ticketsL1OnRoundId(2, user1.address)
+      expect(round1L1U1).to.equal(10);
+      expect(participations[0]).to.equal(1);
+      expect(participations[1]).to.equal(2);
+      expect(participations.length).to.equal(2);
+      expect(round2L1U1).to.equal(2);
+    });
+    it("should set the appropriate tickets when going above max tickets for the round and overflowing", async () => {
+      const { lottery, usdc, user1, user2 } = await loadFixture(lotteryStart);
+      await usdc.connect(user1).approve(lottery.address, parseEther("10000"));
+      await lottery.connect(user1).buyTickets(10000, zero);
+      const participations = await lottery.allRoundsParticipatedIn(user1.address)
+      expect(participations.length).to.equal(12)
+      for(let i = 1; i <= 12; i++){
+        expect(await lottery.ticketsL1OnRoundId(i, user1.address)).to.equal(1000);
+      }
+      expect(await lottery.maxRoundIdPerLevel(1)).to.equal(13);
+    })
+    it("should set the current round to be able to request plays")
+    it("should allow a third party to buy tickets for another user")
+  });
+  
 })
