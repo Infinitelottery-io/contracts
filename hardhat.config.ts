b31b514238d413dd99d68b8dd3244e9ade95b2a8
Semi Invader <=>
2023-07-10 21:49:23 -0600

    add foundry to make the switch

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
