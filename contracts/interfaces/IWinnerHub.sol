5ae3f94123c9f47e593be77823aef7ae2cd68104
Semi Invader <=>
2023-07-10 21:53:28 -0600

    open pragma for interfaces

--- contracts/interfaces/IWinnerHub.sol
+++ contracts/interfaces/IWinnerHub.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.0;
 
 interface IWinnerHub {
-
-  /**
-   * 
-   * @param _winner The address of the winner of the round
-   * @param _referral The referrer of the winner
-   * @param _winAmount Amount designated to the winner
-   * @param _refAmount Amount designated to the referrer
-   */
-  function distributeWinnings(address _winner, address _referral, uint _winAmount, uint _refAmount) external;
-
+    /**
+     *
+     * @param _winner The address of the winner of the round
+     * @param _referral The referrer of the winner
+     * @param _winAmount Amount designated to the winner
+     * @param _refAmount Amount designated to the referrer
+     */
+    function distributeWinnings(
+        address _winner,
+        address _referral,
+        uint _winAmount,
+        uint _refAmount
+    ) external;
 }
